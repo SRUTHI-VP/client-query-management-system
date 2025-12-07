7a0320332517956828ceaeb8678b394df8708b23
SRUTHI-VP <sruthiravindranvp@gmail.com>
2025-12-07 22:40:00 +0530

    Delete Client_Query_Management_System directory

--- Client_Query_Management_System/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",           
-        user="enter your username",        
-        password="enter your password",
-        database="enter your database name"
-    )
